3e3b86b140c40a3190f90a66de54ee0de6011504
IvakaDivaka862 <167872340+IvakaDivaka862@users.noreply.github.com>
2025-07-06 19:37:12 +0300

    Update init.lua

--- client/init.lua
+++ client/init.lua
@@ -1,15 +1,3 @@
-local function NPCSpawn()
-local PlayerPedID = PlayerPedId()
-local PlayerCoords = GetEntityCoords(PlayerPedID)
-local PlayerHeading = GetEntityHeading(PlayerPedID)
-local distance3 = 3
-local NPCcoords = PlayerCoords+vec3(distance3, distance3, .5)
-local PedHash = `a_m_o_soucent_03`
-
-    RequestModel(PedHash)
-    while not HasModelLoaded(PedHash) do
-        Wait(10)
-    end
 
 local function ConfigurePed(PedModel, PlayerPed)
     TaskFollowToOffsetOfEntity(PedModel, PlayerPed, 0.0, 0.0, 0.0, 2.0, -1, 3.0, true)
@@ -18,10 +6,37 @@ local function ConfigurePed(PedModel, PlayerPed)
     SetPedCanBeTargetted(PedModel, false)
     SetBlockingOfNonTemporaryEvents(PedModel, true)
 end
+local Peds = {}
+local function NPCSpawn()
+    local PlayerPedID = PlayerPedId()
+    local PlayerCoords = GetEntityCoords(PlayerPedID)
+    local PlayerHeading = GetEntityHeading(PlayerPedID)
+    local distance3 = 3
+    local NPCcoords = PlayerCoords+vec3(distance3, distance3, .5)
+    local PedHash = `a_m_o_soucent_03`
+
+    RequestModel(PedHash)
+    while not HasModelLoaded(PedHash) do
+        Wait(10)
+    end
 
     for i = 1, 10 do
         local ped = CreatePed(4, PedHash, NPCcoords.x, NPCcoords.y, NPCcoords.z, PlayerHeading, true, false)
         ConfigurePed(ped, PlayerPedID)
+        table.insert(Peds, ped)
+    end
+end
+
+local function NPCStop()
+    for i = 1, #Peds do
+    ClearPedTasks(Peds[i])
+    end
+end
+
+local function NPCdelete()
+    for i = 1, #Peds do
+        DeleteEntity(Peds[i])
+        Peds[i] = nil
     end
 end
 
@@ -30,7 +45,12 @@ local function ScriptStarted()
 end
 
 RegisterCommand('npcspawn', function()
-    NPCSpawn() 
+    NPCSpawn()
+end, false)
+
+RegisterCommand('npcstopfollow', function()
+    NPCStop()
+    NPCdelete()
 end, false)
 
 ScriptStarted()
